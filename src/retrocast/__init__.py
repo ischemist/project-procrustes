20dd8c5c4925e4912e6af1bdc05123a2555e5842
Anton Morgunov <anton.morgunov@yale.edu>
2025-11-15 12:38:29 -0500

    ci: use setuptools-scm for versioning, add version to the route, update workflows

--- src/retrocast/__init__.py
+++ src/retrocast/__init__.py
@@ -2,6 +2,8 @@
 retrocast: A unified toolkit for retrosynthesis benchmark analysis.
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from retrocast.adapters import ADAPTER_MAP, adapt_routes, adapt_single_route, get_adapter
 from retrocast.domain.tree import deduplicate_routes, sample_k_by_depth, sample_random_k, sample_top_k
 from retrocast.schemas import Molecule, ReactionStep, Route, TargetInput
@@ -9,7 +11,11 @@ from retrocast.utils.logging import setup_logging
 
 setup_logging()
 
-__version__ = "0.1.0"
+try:
+    __version__ = version("retrocast")
+except PackageNotFoundError:
+    # Package not installed (running from source without editable install)
+    __version__ = "0.0.0.dev0+unknown"
 __all__ = [
     # Core schemas
     "Route",
