bb148f3bf36d95dda19cb21bd19bee30ccf08bcb
Anton Morgunov <anton.morgunov@yale.edu>
2025-12-01 12:53:00 -0500

    dev: upd cls script

--- scripts/sync-cluster.sh
+++ scripts/sync-cluster.sh
@@ -3,17 +3,23 @@
 set -e
 
 SERVERS=(
-    # "ubuntu@129.213.94.122"
+    "ubuntu@157.151.200.193"
     # "ubuntu@193.122.153.180"
     # "ubuntu@129.213.82.162"
-    "aws-spot-1"
+    # "aws-spot-1"
     # "aws-spot-2"
     # "aws-spot-3"
     # "aws-spot-4"
 )
 
 FILES=(
-    "uspto-190.json.gz"
+    # "uspto-190.json.gz"
+    # "mkt-cnv-160.json.gz"
+    # "mkt-lin-500.json.gz"
+    # "ref-cnv-400.json.gz"
+    # "ref-lin-600.json.gz"
+    # "ref-lng-84.json.gz"
+    "random-n5-50.json.gz"
 )
 STOCK_FILES=(
     "buyables-stock.hdf5"
@@ -27,12 +33,10 @@ STOCK_FILES=(
 
 for server in "${SERVERS[@]}"; do
     echo "Syncing to ${server}..."
-    for stock_file in "${STOCK_FILES[@]}"; do
-        rsync -avz data/1-benchmarks/stocks/${stock_file} "${server}:~/project-procrustes/data/1-benchmarks/stocks/"
+    rsync -avz data/1-benchmarks/stocks "${server}:~/project-procrustes/data/1-benchmarks/"
+    for file in "${FILES[@]}"; do
+        rsync -avz data/1-benchmarks/definitions/${file} "${server}:~/project-procrustes/data/1-benchmarks/definitions/"
     done
-    # for file in "${FILES[@]}"; do
-    #     rsync -avz data/1-benchmarks/definitions/${file} "${server}:~/project-procrustes/data/1-benchmarks/definitions/"
-    # done
 done
 
 echo "Done!"
